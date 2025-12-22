24da3ddf1982111b8c305f7a4938eedd9f6204ae
Pedro Gonzalez <pdro74@hotmail.com>
2025-12-22 08:32:39 +0100

    add passing object fields with thymeleaf

---
+++ src/main/java/com/pgf/thymeleaf_demo/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.pgf.thymeleaf_demo.controller;
+
+import com.pgf.thymeleaf_demo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+
+        Student theStudent = new Student();
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(
+            @ModelAttribute("student") Student theStudent
+    ) {
+
+        System.out.println("the student: " + theStudent.getFirstName() + ", " + theStudent.getLastName());
+        return "student-confirmation";
+    }
+}
